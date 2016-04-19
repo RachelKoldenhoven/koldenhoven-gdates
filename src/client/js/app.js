663d4243e7052126cf413f27b7eee82ffeba5150
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-04-19 15:35:27 -0600

    built register template, logging data from form

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,3 +2,38 @@
  * Created by rachelkoldenhoven on 4/19/16.
  */
 var app = angular.module('gDates', ['ngRoute', 'ngSanitize']);
+
+
+app.config(function($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/home.html',
+      restricted: true,
+      preventLoggedIn: false
+    })
+    .when('/register',{
+      templateUrl: 'templates/register.html',
+      controller: 'registerController',
+      restricted: false,
+      preventLoggedIn: true
+    })
+    .when('/login', {
+      templateUrl: 'templates/login.html',
+      controller: 'loginController',
+      restricted: false,
+      preventLoggedIn: true
+    })
+    .when('/logout', {
+      restricted: false,
+      preventLoggedIn: false,
+      resolve: {
+        test: function(authService, $rootScope, $location) {
+          authService.logout();
+          $rootScope.currentUser = authService.getUserInfo();
+          $location.path('/login');
+        }
+      }
+    })
+    .otherwise({redirectTo: '/login'});
+  $httpProvider.interceptors.push('authInterceptor');
+});
--- src/client/js/controllers.js
+++ src/client/js/controllers.js
@@ -5,3 +5,21 @@ app.controller('navController', ['$rootScope', '$scope', 'authService',
   function($rootScope, $scope, authService){
     $rootScope.currentUser = authService.getUserInfo();
   }]);
+
+
+app.controller('registerController', ['$scope', '$location', 'authService',
+  function($scope, $location, authService) {
+    $scope.user = {};
+    $scope.register = function() {
+      console.log($scope.user);
+      authService.register($scope.user)
+        .then(function(user) {
+          authService.setUserInfo(user);
+          $location.path('/');
+        })
+        .catch(function(err) {
+          // check status code, send appropriate message
+          console.log(err);
+        });
+    };
+  }]);
